f0552960e3b9079121ec282c8a5861e1724f8319
viGor027 <vigor.wm@yahoo.com>
2024-02-03 15:23:22 +0100

    added shape class to manage drawn object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import json
+from shape import Shape
 
 # Initialize Pygame
 pygame.init()
@@ -36,53 +37,30 @@ def de_casteljau(t, points):
         return de_casteljau(t, new_points)
 
 
-# Function to find the index of the closest control point
-def find_closest_point(mouse_pos, current_curve=None):
-    min_distance = float('inf')
-    closest_index = None
-    curve_id = None
-    if current_curve:
-        for i, point in enumerate(all_curves[current_curve]):
-            distance = ((mouse_pos[0] - point[0]) ** 2 + (mouse_pos[1] - point[1]) ** 2) ** 0.5
-            if distance < min_distance:
-                min_distance = distance
-                closest_index = i
-        return closest_index, current_curve
-    else:
-        for arr_id, control_points in enumerate(all_curves):
-            for i, point in enumerate(control_points):
-                distance = ((mouse_pos[0] - point[0]) ** 2 + (mouse_pos[1] - point[1]) ** 2) ** 0.5
-                if distance < min_distance:
-                    min_distance = distance
-                    closest_index = i
-                    curve_id = arr_id
-        return closest_index, curve_id
-
-
 smallfont = pygame.font.SysFont('Corbel', 14)
 
 text = smallfont.render('Nowa prosta', True, BLACK)
 text2 = smallfont.render('Siatka', True, BLACK)
 text3 = smallfont.render('Zapisz', True, BLACK)
-text4 = smallfont.render('Zmien prostą', True, BLACK)
 text5 = smallfont.render('Edycja On ', True, BLACK)
 text6 = smallfont.render('Edycja Off', True, BLACK)
 text7 = smallfont.render('Wyswietl litere', True, BLACK)
 
-# letter_img = pygame.image.load('letters_raw_extracted/n.png')
-letter_img = pygame.image.load('dummy_background.png')
+letter_img = pygame.image.load('letters_raw_extracted/n.png')
+# letter_img = pygame.image.load('dummy_background.png')
 
 # Main game loop
 running = True
 dragging = False
 dragged_point_index = None
-dragged_point_curve_id = None
 draw_grid = True
-edit = False
+edit = True
 show_letter = True
 
 current_curve_id = 0
 prev_edited_curve_id = None
+
+letter = Shape()
 
 # read curves from file
 # f = open('letters_json/n.json')
@@ -127,64 +105,35 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1 and 10 <= mouse[0] <= 85 and 10 <= mouse[1] <= 50:  # kliknięto "Nowa prosta"
-                current_curve_id += 1
-                all_curves.append([])
+                letter.new_curve()
             elif event.button == 1 and 95 <= mouse[0] <= 170 and 10 <= mouse[1] <= 50:  # kliknieto "siatka"
                 draw_grid = not draw_grid
             elif event.button == 1 and 10 <= mouse[0] <= 85 and 60 <= mouse[1] <= 100:  # kliknieto "zapisz"
-                with open('nowa.json', 'w', encoding='utf-8') as f:
-                    json.dump(all_curves, f, ensure_ascii=False, indent=4)
-            elif event.button == 1 and 95 <= mouse[0] <= 170 and 60 <= mouse[1] <= 100:  # kliknieto "zmien prosta"
-                edit = True
-            elif event.button == 1 and 10 <= mouse[0] <= 170 and 110 <= mouse[1] <= 150:  # kliknieto "tryb edycji"
-                if current_curve_id is not None:
-                    prev_edited_curve_id = current_curve_id
-                    current_curve_id = None
-                else:
-                    current_curve_id = prev_edited_curve_id
-            elif event.button == 1 and 10 <= mouse[0] <= 170 and 160 <= mouse[1] <= 200:
+                letter.save('nowa.json')
+            elif event.button == 1 and 95 <= mouse[0] <= 170 and 60 <= mouse[1] <= 100:  # kliknieto "tryb edycji"
+                edit = not edit
+            elif event.button == 1 and 10 <= mouse[0] <= 170 and 120 <= mouse[1] <= 150:
                 show_letter = not show_letter
-            elif event.button == 1 and current_curve_id is not None:  # Left mouse button
-                if len(all_curves) == 0:
-                    all_curves.append([])
-                    current_curve_id += 1
-                    prev_edited_curve_id += 1
-                all_curves[current_curve_id].append(event.pos)
-            elif event.button == 3 and current_curve_id is not None:  # Right mouse button
-                closest_index, closest_curve_id = find_closest_point(event.pos)
+            elif event.button == 1 and edit:  # Left mouse button
+                letter.add_point(event.pos)
+            elif event.button == 3 and edit:  # Right mouse button
+                closest_index = letter.get_closest_point(event.pos)[0]
                 if closest_index is not None:
                     dragging = True
                     dragged_point_index = closest_index
-                    dragged_point_curve_id = closest_curve_id
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 3:  # Right mouse button
                 dragging = False
                 dragged_point_index = None
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and current_curve_id is not None:
-                closest_index, closest_curve_id = find_closest_point(pygame.mouse.get_pos(), current_curve_id)
-                if closest_curve_id is not None and closest_index is not None:
-                    all_curves[closest_curve_id].pop(closest_index)
-                    if len(all_curves[current_curve_id]) == 0:
-                        all_curves.pop(current_curve_id)
-                        prev_edited_curve_id = len(all_curves) - 2
-                        current_curve_id = len(all_curves) - 1
-            elif event.key == pygame.K_e and edit:
-                prev_edited_curve_id = current_curve_id
-                _, current_curve_id = find_closest_point(pygame.mouse.get_pos())
-                edit = False
+            if event.key == pygame.K_SPACE and edit:
+                letter.delete_point(pygame.mouse.get_pos())
+            elif event.key == pygame.K_e:
+                letter.switch_curve(pygame.mouse.get_pos())
             elif event.key == pygame.K_i:
                 img_x, img_y = pygame.mouse.get_pos()
             elif event.key == pygame.K_n:
-
-                if (prev_edited_curve_id is None and len(all_curves[current_curve_id]) > 0) or \
-                   (prev_edited_curve_id is not None and len(all_curves[prev_edited_curve_id]) > 0):
-
-                    point_to_copy = all_curves[current_curve_id][-1]
-                    prev_edited_curve_id = current_curve_id
-                    current_curve_id = len(all_curves)
-                    all_curves.append([])
-                    all_curves[current_curve_id].append(all_curves[prev_edited_curve_id][-1])
+                letter.c0_connection()
 
     mouse = pygame.mouse.get_pos()
     pygame.draw.rect(screen, BUTTON_COLOR, [10, 10, 75, 40])  # nowa prosta button
@@ -193,37 +142,39 @@ while running:
     else:
         pygame.draw.rect(screen, BUTTON_INACTIVE_COLOR, [95, 10, 75, 40])
     pygame.draw.rect(screen, BUTTON_COLOR, [10, 60, 75, 40])  # zapisz button
-    pygame.draw.rect(screen, BUTTON_COLOR, [95, 60, 75, 40])  # edytuj button
-    if current_curve_id is not None:  # tryb edycji button
+    # pygame.draw.rect(screen, BUTTON_COLOR, [95, 60, 75, 40])  # edytuj button
+    if edit:  # tryb edycji button
+        pygame.draw.rect(screen, BUTTON_COLOR, [95, 60, 75, 40])
+    else:
+        pygame.draw.rect(screen, BUTTON_INACTIVE_COLOR, [95, 60, 75, 40])
+
+    if show_letter:
         pygame.draw.rect(screen, BUTTON_COLOR, [10, 110, 160, 40])
     else:
         pygame.draw.rect(screen, BUTTON_INACTIVE_COLOR, [10, 110, 160, 40])
 
-    if show_letter:
-        pygame.draw.rect(screen, BUTTON_COLOR, [10, 160, 160, 40])
-    else:
-        pygame.draw.rect(screen, BUTTON_INACTIVE_COLOR, [10, 160, 160, 40])
-
     if dragging and pygame.mouse.get_pressed()[2]:  # Right mouse button held down
-        all_curves[dragged_point_curve_id][dragged_point_index] = pygame.mouse.get_pos()
+        print(dragged_point_index)
+        letter.update_point(dragged_point_index, pygame.mouse.get_pos())
 
+    shape = letter.get_shape()
     # Draw control points
-    if current_curve_id is not None:
-        for control_points_id in range(len(all_curves)):
-            for point_id in range(len(all_curves[control_points_id])):
-                if control_points_id == current_curve_id:
-                    pygame.draw.circle(screen, ACTIVE_POINT_COLOR, all_curves[control_points_id][point_id],
+    if edit:
+        for control_points_id in range(len(shape)):
+            for point_id in range(len(shape[control_points_id])):
+                if control_points_id == letter.get_current_curve_id() and edit:
+                    pygame.draw.circle(screen, ACTIVE_POINT_COLOR, shape[control_points_id][point_id],
                                        CONTROL_POINT_RADIUS)
                 else:
-                    pygame.draw.circle(screen, BLACK, all_curves[control_points_id][point_id], CONTROL_POINT_RADIUS)
+                    pygame.draw.circle(screen, BLACK, shape[control_points_id][point_id], CONTROL_POINT_RADIUS)
 
     # Draw Bezier curve using de Casteljau's algorithm
-    for control_points_id in range(len(all_curves)):
-        if len(all_curves[control_points_id]) >= 2:
-            for t in range(0, 80*len(all_curves[control_points_id])+1):
-                t /= 80 * len(all_curves[control_points_id])
-                p = de_casteljau(t, all_curves[control_points_id])
-                if control_points_id == current_curve_id:
+    for control_points_id in range(len(shape)):
+        if len(shape[control_points_id]) >= 2:
+            for t in range(0, 80*len(shape[control_points_id])+1):
+                t /= 80 * len(shape[control_points_id])
+                p = de_casteljau(t, shape[control_points_id])
+                if control_points_id == letter.get_current_curve_id() and edit:
                     pygame.draw.circle(screen, ACTIVE_CURVE_COLOR, (int(p[0]), int(p[1])), 1)
                 else:
                     pygame.draw.circle(screen, BLACK, (int(p[0]), int(p[1])), 1)
@@ -231,12 +182,12 @@ while running:
     screen.blit(text, (12, 23))
     screen.blit(text2, (12 + 105, 23))
     screen.blit(text3, (12 + 15, 23 + 50))
-    screen.blit(text4, (12 + 83, 23 + 50))
-    if current_curve_id is not None:
-        screen.blit(text5, (12 + 48, 23 + 100))
+    if edit:
+        #  [95, 60, 75, 40]
+        screen.blit(text5, (12 + 95, 23 + 50))
     else:
-        screen.blit(text6, (12 + 48, 23 + 100))
-    screen.blit(text7, (12 + 39, 23 + 150))
+        screen.blit(text6, (12 + 95, 23 + 50))
+    screen.blit(text7, (12 + 39, 23 + 100))
 
     pygame.display.flip()
     pygame.display.update()
---
+++ shape.py
@@ -0,0 +1,131 @@
+import json
+
+
+class Shape:
+    def __init__(self):
+        self._shape: list[list] = []
+        self._current_curve_id: int = -1
+        self._prev_edited_curve: list = []
+
+    def new_curve(self):
+        """
+        Adds another curve to the shape on click of a user
+        :return: None
+        """
+        if len(self._shape) > 0 and not self._shape[-1]:
+            self._current_curve_id = len(self._shape) - 1
+        else:
+            self._shape.append([])
+            self._prev_edited_curve.append(self._current_curve_id)
+            self._current_curve_id += 1
+
+    def add_point(self, point: tuple):
+        """
+        :param point: point to be added to curve
+        :return: None
+        """
+        if self._current_curve_id != -1:
+            self._shape[self._current_curve_id].append(point)
+
+    def update_point(self, point_id: int, pos: tuple):
+        """
+        Updates dragged point cords.
+        :param point_id: id of a point on currently edited curve that is being dragged
+        :param pos: new position of a point on current curve with id point_id
+        :return:  None
+        """
+        self._shape[self._current_curve_id][point_id] = [pos[0], pos[1]]
+
+    def save(self, directory: str):
+        """
+        :param directory: the place where the json file with the shape will be saved, including name of the file and
+        *.extension
+        :return: None
+        """
+        with open(directory, 'w', encoding='utf-8') as f:
+            json.dump(self._shape, f, ensure_ascii=False, indent=4)
+
+    def get_closest_point(self, mouse_pos: tuple, through_all: bool = False):
+        """
+        :param through_all:  If true find the closest point from all the curves,
+                             else find the closest point on currently edited curve
+        :param mouse_pos: position of a mouse
+        :return: id of closest point to the mouse on currently edited curve if through_all is True,
+                 else id of closest point to the mouse from all the curves
+        """
+        if through_all:
+            return self._search_through_all_curves(mouse_pos)
+
+        if self._current_curve_id == -1:
+            return None, -1
+
+        min_distance = float('inf')
+        closest_point_id = None
+        for i, point in enumerate(self._shape[self._current_curve_id]):
+            distance = ((mouse_pos[0] - point[0]) ** 2 + (mouse_pos[1] - point[1]) ** 2) ** 0.5
+            if distance < min_distance:
+                min_distance = distance
+                closest_point_id = i
+        return closest_point_id, self._current_curve_id
+
+    def _search_through_all_curves(self, mouse_pos: tuple):
+        """
+        :param mouse_pos: position of a mouse
+        :return: id of closest point to the mouse from all the curves and id of its curve
+        """
+        closest_point_id = None
+        closest_curve_id = None
+        min_distance = float('inf')
+        for curve_id, curve in enumerate(self._shape):
+            for point_id, point in enumerate(curve):
+                distance = ((mouse_pos[0] - point[0]) ** 2 + (mouse_pos[1] - point[1]) ** 2) ** 0.5
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_point_id = point_id
+                    closest_curve_id = curve_id
+        return closest_point_id, closest_curve_id
+
+    def switch_curve(self, mouse_pos: tuple):
+        """
+        Changes the currently edited curve to a different one
+        :param mouse_pos: position of a mouse
+        :return: None
+        """
+        closest_point_id, closest_curve_id = self._search_through_all_curves(mouse_pos)
+        self._prev_edited_curve.append(self._current_curve_id)
+        self._current_curve_id = closest_curve_id
+
+    def delete_point(self, mouse_pos: tuple):
+        """
+        deletes a point from currently edited curve
+        :param mouse_pos:  position of mouse
+        :return: None
+        """
+        closest_point_id, currently_edited_curve_id = self.get_closest_point(mouse_pos)
+        if currently_edited_curve_id != -1:
+            self._shape[currently_edited_curve_id].pop(closest_point_id)
+            if len(self._shape[currently_edited_curve_id]) == 0:
+                self._shape.pop(currently_edited_curve_id)
+                self._current_curve_id = self._prev_edited_curve.pop(-1)
+
+    def c0_connection(self):
+        """
+        Creates new curve with its first point same as last point of current curve
+        :return: None
+        """
+        if self._current_curve_id != -1:
+            point_to_be_copied = self._shape[self._current_curve_id][-1]
+            self.new_curve()
+            self.add_point(point_to_be_copied)
+
+    def get_current_curve_id(self):
+        """
+        :return: id of a currently edited curve
+        """
+        return self._current_curve_id
+
+    def get_shape(self):
+        """
+        :return: array of curves that make up a shape.
+        """
+        return self._shape
