3d01b222059dd136a7bd2a44353015314f80d634
viGor027 <vigor.wm@yahoo.com>
2024-02-04 15:24:56 +0100

    restructurized folder structure

--- type.py
+++ type.py
@@ -76,7 +76,7 @@ def draw_letter(letter, shift_y, scale, shift_x=0):
     return prev_begin_from
 
 
-letter = open('letters_json/a.json')
+letter = open('resources/letters_json/a.json')
 a = json.load(letter)
 all_letters = []
 letters_copy = []
@@ -133,7 +133,7 @@ while running:
                 make_cursive(cursive)
                 cursive *= -1
             elif keys[pygame.K_z] and keys[pygame.K_RALT]:
-                letter = open('letters_json/ż.json')
+                letter = open('resources/letters_json/ż.json')
                 z = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(z, 0, .38, -15)
@@ -141,7 +141,7 @@ while running:
                     draw_letter(z, 0, .38)
                 last_letter.append('ż')
             elif keys[pygame.K_x] and keys[pygame.K_RALT]:
-                letter = open('letters_json/ź.json')
+                letter = open('resources/letters_json/ź.json')
                 z = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(z, 0, .38, -15)
@@ -150,7 +150,7 @@ while running:
                     draw_letter(z, 0, .38)
                 last_letter.append('j')
             elif keys[pygame.K_e] and keys[pygame.K_RALT]:
-                letter = open('letters_json/ę.json')
+                letter = open('resources/letters_json/ę.json')
                 e = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(e, 46, .27, -2)
@@ -158,17 +158,17 @@ while running:
                     draw_letter(e, 46, .27)
                 last_letter.append('ę')
             elif keys[pygame.K_a] and keys[pygame.K_RALT]:
-                letter = open('letters_json/ą.json')
+                letter = open('resources/letters_json/ą.json')
                 a = json.load(letter)
                 draw_letter(a, 44, .29)
                 last_letter.append('ą')
             elif keys[pygame.K_o] and keys[pygame.K_RALT]:
-                letter = open('letters_json/ó.json')
+                letter = open('resources/letters_json/ó.json')
                 o = json.load(letter)
                 draw_letter(o, 9, .39)
                 last_letter.append('ó')
             elif keys[pygame.K_l] and keys[pygame.K_RALT]:
-                letter = open('letters_json/ł.json')
+                letter = open('resources/letters_json/ł.json')
                 l = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(l, -25, .4, -20)
@@ -178,12 +178,12 @@ while running:
                     draw_letter(l, -25, .4, -15)
                 last_letter.append('ł')
             elif keys[pygame.K_s] and keys[pygame.K_RALT]:
-                letter = open('letters_json/ś.json')
+                letter = open('resources/letters_json/ś.json')
                 s = json.load(letter)
                 draw_letter(s, 25, .33)
                 last_letter.append('ś')
             elif keys[pygame.K_c] and keys[pygame.K_RALT]:
-                letter = open('letters_json/ć.json')
+                letter = open('resources/letters_json/ć.json')
                 c = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(c, 50, .25, -5)
@@ -191,7 +191,7 @@ while running:
                     draw_letter(c, 50, .25)
                 last_letter.append('ć')
             elif keys[pygame.K_n] and keys[pygame.K_RALT]:
-                letter = open('letters_json/ń.json')
+                letter = open('resources/letters_json/ń.json')
                 n = json.load(letter)
                 draw_letter(n, 18, .37)
                 last_letter.append('ń')
@@ -201,17 +201,17 @@ while running:
                 begin_from += 50
                 last_letter.append('j')
             elif event.key == pygame.K_a:
-                letter = open('letters_json/a.json')
+                letter = open('resources/letters_json/a.json')
                 a = json.load(letter)
                 draw_letter(a, 40, .3)
                 last_letter.append('a')
             elif event.key == pygame.K_b:
-                letter = open('letters_json/b.json')
+                letter = open('resources/letters_json/b.json')
                 b = json.load(letter)
                 draw_letter(b, 4, .32)
                 last_letter.append('b')
             elif event.key == pygame.K_c:
-                letter = open('letters_json/c.json')
+                letter = open('resources/letters_json/c.json')
                 c = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(c, 50, .25, -5)
@@ -219,7 +219,7 @@ while running:
                     draw_letter(c, 50, .25)
                 last_letter.append('c')
             elif event.key == pygame.K_d:
-                letter = open('letters_json/d.json')
+                letter = open('resources/letters_json/d.json')
                 d = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(d, -2, .36, -2)
@@ -227,7 +227,7 @@ while running:
                     draw_letter(d, -2, .36)
                 last_letter.append('d')
             elif event.key == pygame.K_e:
-                letter = open('letters_json/e.json')
+                letter = open('resources/letters_json/e.json')
                 e = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(e, 46, .27, -2)
@@ -235,7 +235,7 @@ while running:
                     draw_letter(e, 46, .27)
                 last_letter.append('e')
             elif event.key == pygame.K_f:
-                letter = open('letters_json/f.json')
+                letter = open('resources/letters_json/f.json')
                 f = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(f, -8, .5, -10)
@@ -243,7 +243,7 @@ while running:
                     draw_letter(f, -8, .5)
                 last_letter.append('f')
             elif event.key == pygame.K_g:
-                letter = open('letters_json/g.json')
+                letter = open('resources/letters_json/g.json')
                 g = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(g, 18, .5, -5)
@@ -251,34 +251,34 @@ while running:
                     draw_letter(g, 18, .5)
                 last_letter.append('g')
             elif event.key == pygame.K_h:
-                letter = open('letters_json/h.json')
+                letter = open('resources/letters_json/h.json')
                 h = json.load(letter)
                 draw_letter(h, -60, .5)
                 last_letter.append('h')
             elif event.key == pygame.K_i:
-                letter = open('letters_json/i.json')
+                letter = open('resources/letters_json/i.json')
                 i = json.load(letter)
                 draw_letter(i, -30, 0.45)
                 last_letter.append('i')
             elif event.key == pygame.K_j:
-                letter = open('letters_json/j.json')
+                letter = open('resources/letters_json/j.json')
                 j = json.load(letter)
                 if len(all_letters) == 0:
                     begin_from += 40
                 draw_letter(j, -110, 0.8, -35)
                 last_letter.append('j')
             elif event.key == pygame.K_k:
-                letter = open('letters_json/k.json')
+                letter = open('resources/letters_json/k.json')
                 k = json.load(letter)
                 draw_letter(k, -40, .5)
                 last_letter.append('k')
             elif event.key == pygame.K_l:
-                letter = open('letters_json/l.json')
+                letter = open('resources/letters_json/l.json')
                 l = json.load(letter)
                 draw_letter(l, -25, .4)
                 last_letter.append('l')
             elif event.key == pygame.K_m:
-                letter = open('letters_json/m.json')
+                letter = open('resources/letters_json/m.json')
                 m = json.load(letter)
                 if last_letter and last_letter[-1] in ['a', 'n']:
                     draw_letter(m, 18, 0.37, -8)
@@ -286,17 +286,17 @@ while running:
                     draw_letter(m, 18, 0.37)
                 last_letter.append('m')
             elif event.key == pygame.K_n:
-                letter = open('letters_json/n.json')
+                letter = open('resources/letters_json/n.json')
                 n = json.load(letter)
                 draw_letter(n, 18, .37)
                 last_letter.append('n')
             elif event.key == pygame.K_o:
-                letter = open('letters_json/o.json')
+                letter = open('resources/letters_json/o.json')
                 o = json.load(letter)
                 draw_letter(o, 9, .39)
                 last_letter.append('o')
             elif event.key == pygame.K_p:
-                letter = open('letters_json/p.json')
+                letter = open('resources/letters_json/p.json')
                 p = json.load(letter)
                 if len(all_letters) == 0:
                     begin_from += 40
@@ -308,22 +308,22 @@ while running:
                     draw_letter(p, -5, 0.52)
                 last_letter.append('p')
             elif event.key == pygame.K_q:
-                letter = open('letters_json/q.json')
+                letter = open('resources/letters_json/q.json')
                 q = json.load(letter)
                 draw_letter(q, 15, 0.5)
                 last_letter.append('q')
             elif event.key == pygame.K_r:
-                letter = open('letters_json/r.json')
+                letter = open('resources/letters_json/r.json')
                 r = json.load(letter)
                 draw_letter(r, 25, .33)
                 last_letter.append('r')
             elif event.key == pygame.K_s:
-                letter = open('letters_json/s.json')
+                letter = open('resources/letters_json/s.json')
                 s = json.load(letter)
                 draw_letter(s, 25, .33)
                 last_letter.append('s')
             elif event.key == pygame.K_t:
-                letter = open('letters_json/t.json')
+                letter = open('resources/letters_json/t.json')
                 t = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(t, -32, .45, -15)
@@ -333,22 +333,22 @@ while running:
                     draw_letter(t, -32, .45, -10)
                 last_letter.append('t')
             elif event.key == pygame.K_u:
-                letter = open('letters_json/u.json')
+                letter = open('resources/letters_json/u.json')
                 u = json.load(letter)
                 draw_letter(u, 20, .35)
                 last_letter.append('u')
             elif event.key == pygame.K_v:
-                letter = open('letters_json/v.json')
+                letter = open('resources/letters_json/v.json')
                 v = json.load(letter)
                 draw_letter(v, 23, .35)
                 last_letter.append('v')
             elif event.key == pygame.K_w:
-                letter = open('letters_json/w.json')
+                letter = open('resources/letters_json/w.json')
                 w = json.load(letter)
                 draw_letter(w, 18, .37)
                 last_letter.append('w')
             elif event.key == pygame.K_x:
-                letter = open('letters_json/x.json')
+                letter = open('resources/letters_json/x.json')
                 x = json.load(letter)
                 if last_letter and last_letter[-1] == 'j':
                     draw_letter(x, 10, .38, -20)
@@ -356,12 +356,12 @@ while running:
                     draw_letter(x, 10, .38)
                 last_letter.append('x')
             elif event.key == pygame.K_y:
-                letter = open('letters_json/y.json')
+                letter = open('resources/letters_json/y.json')
                 y = json.load(letter)
                 draw_letter(y, 5, .52)
                 last_letter.append('y')
             elif event.key == pygame.K_z:
-                letter = open('letters_json/z.json')
+                letter = open('resources/letters_json/z.json')
                 z = json.load(letter)
                 draw_letter(z, 0, .38)
                 last_letter.append('z')
